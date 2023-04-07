ee76c1359f085856e67c5bdadd97660e7bb5bc51
mdmayda <mdmayda123@gmail.com>
2023-04-07 21:04:59 +0700

    Memperbaiki header, footer, menambah tampilan lain

--- Layout/header.php
+++ Layout/header.php
@@ -1,28 +1,29 @@
-<link rel="stylesheet" href="./style/layout.css">
-<link rel="stylesheet" href="./style/bootsrap.css">
+    <link rel="stylesheet" href="./style/layout.css">
+    <link rel="stylesheet" href="./style/bootsrap.css">
 
-<header>
-    <nav>
-        <a><img src="img\tubes.png" class="logo"></a>
-        <ul class="nav-ul">
-            <li class="nav-item active">
-                <a class="nav-link" href="home.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="dashboard.php">Dashboard</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="logbook.php">Logbook</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="aboutus.php">About Us</a>
-            </li>
-            <a class="nav-link" href="/"><?php echo $_SESSION['aptk_name'] ?></a>
-            <a>
-                <img class = "profile_photo" src= "img\pngwing.com.png" >
-            </a>
-        </ul>
-        </div>
-    </nav>
-</header>
-<script src="../js/bootstrap.bundle.min.js"></script>
+    <header>
+        <nav>
+            <a><img src="img\tubes.png" class="logo"></a>
+            <ul class="nav-ul">
+                <li class="nav-item active">
+                    <a class="nav-link" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="dashboard.php">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logbook.php">Logbook</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="aboutus.php">About</a>
+                </li>
+                <a class="nav-link" href="/"><?php echo $_SESSION['aptk_name'] ?></a>
+                <a>
+                    <img class = "profile_photo" src= "img\pngwing.com.png" >
+                </a>
+                
+            </ul>
+            </div>
+        </nav>
+    </header>
+    <script src="../js/bootstrap.bundle.min.js"></script>
